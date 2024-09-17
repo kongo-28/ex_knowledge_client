06df25e8773015e4d975a179114210b4fec3bfff
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-17 15:49:32 +0900

    knowledgeデータを取得できるよう記述

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,20 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
 
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3000/knowledges");
+  const knowledges = await res.json();
+
+  console.log(knowledges);
+
+  return {
+    props: {
+      knowledges,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+}
+
 export default function Home() {
   return <div>トップページ</div>;
 }
