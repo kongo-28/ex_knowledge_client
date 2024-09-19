284f41180ca13029e2a564fbfc8c37a2c10b83b1
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-19 18:20:16 +0900

    新規登録ページでAPIをたたけるよう編集

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import styles from "@/styles/Home.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const register = () => {
-  return <div>新規登録</div>;
+  // ////////  handleSubmit  ////////////////////////////////////
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(email, password);
+
+    //APIをたたく
+    try {
+      await axios.post("http://localhost:3000/auth", {
+        email: email,
+        password: password,
+      });
+
+      router.push("/"); //リダイレクト
+    } catch (err) {
+      alert("登録に失敗しました");
+    }
+  };
+  // ////////  handleSubmit  ////////////////////////////////////
+
+  ///////////////////////////////////////////////////////////////
+  return (
+    <div className={styles.container}>
+      <p>新規登録</p>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label className={styles.label}>email</label>
+        <input
+          type="text"
+          className={styles.input}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+        <label className={styles.label}>password</label>
+
+        <textarea
+          className={styles.textarea}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+            setPassword(e.target.value)
+          }
+        />
+
+        <button type="submit" className={styles.button}>
+          register!!!
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default register;
